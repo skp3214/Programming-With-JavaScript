5cff0415720f7ae5f7b65470a6c21ef1df8c05d6
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2023-12-03 09:34:15 +0530

    Function one ques added.

--- 3_Functions_Array_Objects/Functions.js
+++ 3_Functions_Array_Objects/Functions.js
@@ -12,4 +12,15 @@ function subTwoNums(a,b){
 subTwoNums(35,4);     // passing arguments to the function
 subTwoNums(3,1);      // passing arguments to the function
 
+function wordMatch(word, match){
+    for(var i=0;i<word.length;i++){
+        if (word[i]===match){
+            console.log("Found at Index: "+i);
+            break;
+        }
+        else console.log("No match Found at index: "+ i)
+    }
 
+}
+
+wordMatch("Master","r")
