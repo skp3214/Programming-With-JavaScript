5e7dd382f29d6f727eba366539c7cfd75123d523
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2023-12-03 10:07:00 +0530

    Math cheat sheet

---
+++ 3_Functions_Array_Objects/MathCheatSheet.js
@@ -0,0 +1,30 @@
+console.log(Math.PI)
+console.log(Number.MAX_VALUE);
+console.log(Math.E);  // Eulers Number
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.ceil(2.333));
+console.log(Math.floor(2.987654321));
+console.log(Math.round(2.987654321));
+console.log(Math.random());
+console.log(Math.trunc(233.4444))
+console.log(Math.pow(2,3))
+console.log(Math.sqrt(16))
+console.log(Math.abs(-10))
+console.log(Math.max(1,2,3,4,5))
+console.log(Math.min(1,2,3,4,5))
+console.log(Math.cbrt(8))
+console.log(Math.log(4))
+console.log(Math.log2(4))
+console.log(Math.log10(4))
+console.log(Math.exp(1))
+console.log(Math.cos(0))
+
+
+// Math Object Methods:
+// toString() - returns a string representation of the number
+// toFixed() - formats the number with specified digits after decimal point
+// toExponential() - returns a string version of the number in exponential notation
+// toPrecision() - returns a string version of the number with specified precision
+
+
