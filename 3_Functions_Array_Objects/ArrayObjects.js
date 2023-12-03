24903986bc4ab5ca661d99a94492f4f0eaa54d6e
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2023-12-03 09:54:48 +0530

    arrays are object intro

---
+++ 3_Functions_Array_Objects/ArrayObjects.js
@@ -0,0 +1,9 @@
+// In JavaScript, arrays are objects. That means that arrays also have some built-in properties and methods.
+// One of the most commonly used built-in methods on arrays are the push() and the pop() methods.
+
+var arr=[]
+for(var i=0;i<10;i++){
+    arr.push(i)
+}
+
+console.log(arr)
