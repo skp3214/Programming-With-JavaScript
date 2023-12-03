0cdf659364e828e5140258448be4d61789d5cbfb
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2023-12-03 09:54:36 +0530

    object intro

---
+++ 3_Functions_Array_Objects/Objects.js
@@ -0,0 +1,25 @@
+// var storeManager={}  empty object
+
+var storeManager={
+    Name:"Sachin",
+    Age:30,
+
+}
+storeManager["Name"]="Sameer"  // this will update the value
+storeManager["Income"]=30000  // it will add a new key value pair if it is already not present.
+storeManager.Income=40000     // this will update the value.
+storeManager.Age=40           // this will update the value.
+
+console.log(storeManager)
+
+var keys=["speed","altitude","color"]
+
+var drone={
+    speed:200,
+    altitude:100,
+    color:"red"
+}
+
+for(var i=0;i<keys.length;i++){
+    console.log(drone[keys[i]])
+}
