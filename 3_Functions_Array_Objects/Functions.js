411a1fc27f726a167d47142ce1253a6174a70660
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2023-12-03 09:22:48 +0530

    funtion intro

---
+++ 3_Functions_Array_Objects/Functions.js
@@ -0,0 +1,15 @@
+function addTwoNums(){    // funcction declaration
+    var a=10
+    var b=20
+    console.log(a+b)
+}
+
+addTwoNums();     // function calling or invokation
+
+function subTwoNums(a,b){
+    console.log(a-b)
+}
+subTwoNums(35,4);     // passing arguments to the function
+subTwoNums(3,1);      // passing arguments to the function
+
+
